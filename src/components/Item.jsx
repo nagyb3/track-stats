aec150d4dc2385323710b7aefa0ef47dd08d7906
nagyb3 <nagybence2003@gmail.com>
2023-07-05 12:01:34 +0200

    fix name on itmes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,7 +6,6 @@ export default function Item(props) {
     
     return (
         <div className="item-container">
-            <p>{props.item.name}</p>
             <p className="name"><a target="_blank" href={props.item.external_urls.spotify}>{props.item.name}</a></p>
             {thisType === 'tracks' ? <p><a target="_blank" href={props.item.artists[0].external_urls.spotify}>{props.item.artists[0].name}</a></p> : undefined}
             {thisType === 'tracks' ? <img src={props.item.album.images[0].url} />
