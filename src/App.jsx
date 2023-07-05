7762428b530ea6c1a9dd7e7bb1e9a8184051f90c
nagyb3 <nagybence2003@gmail.com>
2023-07-05 11:55:50 +0200

    cleanup App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
 import {useState, useEffect} from "react";
-import axios from "axios";
 import "./App.css"
 import NavBar from "./components/NavBar";
-import Item from "./components/Item";
 import Login from "./components/Login";
 import Main from "./components/Main";
 
 function App() {
-    document.title = 'spotify-stats'
+    document.title = 'spotify-stats';
 
-    const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
-    const REDIRECT_URI = "http://localhost:5173/callback"
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-    const RESPONSE_TYPE = "token"
+    const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
-    const [token, setToken] = useState("")
+    // const [token, setToken] = useState("");
     
-    const [searchKey, setSearchKey] = useState("")
-    const [artists, setArtists] = useState([])
-
-    // const [accessToken, setAccessToken] = useState("");
- 
     const [topItems, setTopItems] = useState([]);
 
     const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -32,33 +22,15 @@ function App() {
         if (!code) {
             redirectToAuthCodeFlow(CLIENT_ID);
         } else {
-            // const accessToken = getAccessToken(CLIENT_ID, code);
             getAccessToken(CLIENT_ID, code);
         }
-
-        // console.log('localstorage access-token:', localStorage.getItem('access_token'));
-    
-        // const hash = window.location.hash
-        // let token = window.localStorage.getItem("token")
-        // if (!token && hash) {
-        //     //gets run on login to spotify button
-        //     token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-        //     window.location.hash = ""
-        //     window.localStorage.setItem("token", token)
-        // }
-        // setToken(token);
-        // getProfile(localStorage.getItem('access_token'));
-
-        // getTopItems(localStorage.getItem('access_token'), 'artists', 'short_term');
-        
-    }, [])
+    }, []);
     
     async function getAccessToken(CLIENT_ID, code) {
         const verifier = localStorage.getItem("verifier");
         const paramsSecond = new URLSearchParams();
         
-        paramsSecond.append("client_id", CLIENT_ID)
+        paramsSecond.append("client_id", CLIENT_ID);
         paramsSecond.append("grant_type", "authorization_code");
         paramsSecond.append("code", code);
         paramsSecond.append("redirect_uri", "http://localhost:5173/callback");
@@ -94,68 +66,17 @@ function App() {
         });
       
         const data = await response.json();
-        // console.log(data);
     }
-
-
-    async function getMediumTermTopArtist(accessTokenn) {
-        try {
-            const response = await fetch('https://api.spotify.com/v1/me/top/artists', {
-                headers: {
-                   Authorization: 'Bearer ' + accessTokenn
-                }
-            });
-            const data = await response.json();
-            setTopItems(data.items);
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
 
     const logout = () => {
-        setToken("")
+        // setToken("")
         window.localStorage.removeItem("token")
-        //added
         window.localStorage.removeItem("access_token")
         setTopItems([]);
         setIsLoggedIn(false);
     }
 
-    const searchArtists = async (e) => {
-        e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            params: {
-                q: searchKey,
-                type: "artist"
-            }
-        })
-    
-        setArtists(data.artists.items)
-    }
 
-    const renderArtists = () => {
-        return artists.map(artist => (
-            <div key={artist.id}>
-                {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-                {artist.name}
-            </div>
-        ))
-    }
-
-    const renderTopItems = () => {
-        return topItems.map(item => (
-            <div key={item.id}>
-                {item.name}
-            </div>
-        ))
-    }
-
-    //redirect to spotify login page
-    //better auth !
     async function redirectToAuthCodeFlow(clientId) {
         const verifier = generateCodeVerifier(128);
         const challenge = await generateCodeChallenge(verifier);
