fb69edbac8eb9d1b3ee2037b278b84e4f603c522
nagyb3 <nagybence2003@gmail.com>
2023-07-08 18:43:59 +0200

    treat redirect uri as environment variable

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
         const params = new URLSearchParams();
         params.append("client_id", clientId);
         params.append("response_type", "code");
-        params.append("redirect_uri", "http://localhost:5173/callback");
+        params.append("redirect_uri", import.meta.env.VITE_REDIRECT_URI);
         params.append("scope", "user-top-read user-read-private user-read-email");
         params.append("code_challenge_method", "S256");
         params.append("code_challenge", challenge);
